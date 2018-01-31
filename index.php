e228d8723d6b13bdb5fe127d85898ad3622f4524
Nix2k <mihail.bokov@gmail.com>
2018-01-31 21:51:03 +0300

    -

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-	$animals = ['Africa'->[],'Eurasia'->[],'North America'->[],'South America'->[],'Antarctica'->[],'Australia'->[]];
+	$animals = ['Africa'=>['Giraffa camelopardalis','Hippopotamus amphibius','Syncerus caffer'],'Eurasia'=>['Ursus arctos','Cervus elaphus','Sciurus vulgaris'],'North America'=>['Dasypus novemcinctus','Alligator mississippiensis','Castor canadensis'],'South America'=>['Lama guanicoe','Myrmecophaga tridactyla','Tremarctos ornatus'],'Antarctica'=>['Leptonychotes weddellii','Aptenodytes forsteri','Hydrurga leptonyx'],'Australia'=>['Phascolarctos cinereus','Tachyglossus aculeatus','Myrmecobius fasciatus']];
+	print_r($animals);
 ?>
